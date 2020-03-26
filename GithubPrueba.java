77fbba04cc8acdd0a15795c1ef54e5df44493ba9
0321mportoles <62716251+0321mportoles@users.noreply.github.com>
2020-03-26 19:02:59 +0100

    Haciendo pruebas saludo por nombre a través de variable

--- GithubPrueba.java
+++ GithubPrueba.java
@@ -3,7 +3,7 @@ package github;
 public class GithubPrueba {
 	
 	public static void main(String[] args) {
-	
-		System.out.print("Hola mundo");
+		String nombre = "Marta";
+		System.out.print("Hola " + nombre);
 	}
 }
